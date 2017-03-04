2e70897579783356b5d8cebea372edac913a3742
zhanglizeyi <zhanglizeyi@gmail.com>
2017-03-04 14:20:12 -0800

    add more of hw1 concepts

--- Java/homework_cse12/hw1/HW1 Starter Files/RockPaperScissors.java
+++ Java/homework_cse12/hw1/HW1 Starter Files/RockPaperScissors.java
@@ -8,10 +8,23 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.*;
  
 public class RockPaperScissors
 {
-  
+
+  private static String kkk = "Instance Var";
+ 
+  public void abstruct(){
+  	System.out.println("abstruct func");
+  }
+
+  private RockPaperScissors r = new RockPaperScissors();
+
+  public void print(){
+  	r.abstruct();
+  }
+
   public static void MachinePick(char a, String b){
   	  	if(b.charAt(0) =='r') System.out.println("I choose "+a+". It's a tie");
   		else if(b.charAt(0) == 's') System.out.println("I choose "+a+". You win!");
@@ -54,11 +67,32 @@ public class RockPaperScissors
     LinkedList<String> systemMoves = new LinkedList<String>();  
   	
   	int userInput = 0;
+  	double x = 5; //it auto cast to 5.0
+  	//static can be changed, final can not 
+
+  	    String string = "aasdfb csadfas";
+    	System.out.println(string.split(" "));
+    	System.out.println(string.length());
+    	System.out.println(string.split(" ").length);
+
+    	string.format("%d%s\n",14,"askdfjl"); //vaild statement
+
+    	int xyz = 4;
+    	int test = 4;
+    	try{
+    		for(int i=0; i<initialCapacity+1; i++){
+    			System.out.println(userMoves[i]);
+    		}
+    	}catch(IndexOutOfBoundsException e){
+    		System.out.println("out of bound exp");
+    	}
+    
 
     while(true){
-    	System.out.println("Let's play! WHat's your move? (r=rock, p=paper, s=scissors or q to quit)");
+    	System.out.println("Let's play! WHat's your move? (r=rock, p=paper, s=scissors or q to quit)" + kkk);
     	Scanner in = new Scanner(System.in);
     	char c = in.next().charAt(0);
+    	//test
     	
     	if(c == 'r' || c == 'p' || c == 's'){
 			userMoves[userInput++] = Character.toString(c);
@@ -70,7 +104,7 @@ public class RockPaperScissors
     		System.out.println("Thanks for playing");
     		//print reverse order of userMoves and systemMoves
     		for(int i=userInput-1; i>=0; i--){
-    			System.out.println("me: "+userMoves[i]+ "You:"+ systemMoves.get(i));
+    			System.out.println("me: "+userMoves[i]+ "You:"+ systemMoves.get(i));	
     		}
 
     		break;
@@ -78,6 +112,7 @@ public class RockPaperScissors
     	else{
     		System.out.println("That is not a valid move. Please try again."+ c);
     	}	
+    	kkk = Character.toString(c);
     }
 
   }
