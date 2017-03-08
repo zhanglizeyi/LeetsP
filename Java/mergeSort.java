7d5d178fc9e52848f2da181f7e39bf0649a36d99
zhanglizeyi <zhanglizeyi@gmail.com>
2017-03-08 14:10:18 -0800

    mergesort done

--- Java/mergeSort.java
+++ Java/mergeSort.java
@@ -3,10 +3,62 @@ import java.util.*;
 
 public class mergeSort{
 
+	public static void mergesort(int[] arr, int[] temp, int left, int right){
+		if(left >= right) return; //nothing to do it
+
+		int half = (right + left)/2; 
+		mergesort(arr, temp, left, half);
+		mergesort(arr,temp, half+1, right);
+		//helper func 
+		mergeBack(arr, temp, left, half+1, right);
+	}
+
+	public static void mergeBack(int[] arr, int[] temp, int left, int center, int right){
+
+		int leftEnd = center - 1; //for left half 
+		int start = left; // for new temp array to use
+		int size = right - left + 1;
+
+		while(left <= leftEnd && center <= right){
+            if(arr[left] <= arr[center])
+                temp[start++] = arr[left++];
+            else
+                temp[start++] = arr[center++];
+        }
+
+        while(left <= leftEnd)    // Copy rest of first half
+            temp[start++] = arr[left++];
+
+        while(center <= right)  // Copy rest of right half
+            temp[start++] = arr[center++];
+
+        for(int i = 0; i < size; i++, right--)
+            arr[right] = temp[right];
+
+		//return temp;
+	}
+
+	public static void main(String[] args){
+
+		int[] test = {1, 5, 2, 3, 7, 9, 10, 0 ,8};
+		//mergeSort(test);
+		mergesort(test, new int[test.length], 0, test.length-1);
+
+		int[] test2 = {2, 1, 3};
+		//test2 = merge(test2, new int[test2.length], 0, 1, 2);
+
+		for(int i=0; i<test.length; i++){
+			System.out.println(test[i]);
+		}
+	}
+
+
+
+
 	public static void mergeSort(int [ ] a)
 	{
 		int[] tmp = new int[a.length];
-		mergeSort(a, tmp,  0,  a.length - 1);
+		mergesort(a, tmp,  0,  a.length - 1);
 	}
 
 
@@ -22,17 +74,20 @@ public class mergeSort{
 	}
 
 
-    private static void merge(int[ ] a, int[ ] tmp, int left, int right, int rightEnd )
+    private static int[] merge(int[ ] a, int[ ] tmp, int left, int right, int rightEnd )
     {
-        int leftEnd = right - 1;
-        int k = left;
-        int num = rightEnd - left + 1;
 
-        while(left <= leftEnd && right <= rightEnd)
+    	//0 1 2   with arry {2, 1, 3}
+        int leftEnd = right - 1; // 1
+        int k = left; //0
+        int num = rightEnd - left + 1; // 1
+
+        while(left <= leftEnd && right <= rightEnd){
             if(a[left] <= a[right])
                 tmp[k++] = a[left++];
             else
                 tmp[k++] = a[right++];
+        }
 
         while(left <= leftEnd)    // Copy rest of first half
             tmp[k++] = a[left++];
@@ -43,17 +98,9 @@ public class mergeSort{
         // Copy tmp back
         for(int i = 0; i < num; i++, rightEnd--)
             a[rightEnd] = tmp[rightEnd];
+
+        return tmp; 
     }
 
-
-	public static void main(String[] args){
-
-		int[] test = {1, 5, 2, 3, 7, 9, 10, 0};
-		mergeSort(test);
-
-		for(int i=0; i<test.length; i++){
-			System.out.println(test[i]);
-		}
-
-	}
 }
+
